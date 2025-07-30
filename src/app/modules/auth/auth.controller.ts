3e5ebbf530becf64f00011bbd330f6c0a71d7d16
devsafix <devsafix@gmail.com>
2025-07-30 13:51:51 +0600

    register and login controller created

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { AuthServices } from "./auth.service";
+
+const register = async (req: Request, res: Response) => {
+  const user = await AuthServices.registerUser(req.body);
+  res.status(201).json({ success: true, data: user });
+};
+
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const result = await AuthServices.loginUser(email, password);
+  res.status(200).json({ success: true, data: result });
+};
+
+export const AuthController = { register, login };
