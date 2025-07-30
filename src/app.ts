0193b4feb584bced30771d38967068b66798f9ad
devsafix <devsafix@gmail.com>
2025-07-30 13:59:14 +0600

    global route added and call it on app file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { envVariables } from "./app/config/env";
+import { router } from "./app/routes";
 
 const app: Application = express();
 
@@ -10,6 +11,9 @@ app.use(cors({ origin: envVariables.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// all routes
+app.use("/api/v1/", router);
 
 app.get("/", (req: Request, res: Response) => {
   res
