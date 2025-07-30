b9a0f92aedae531d600db82613fe8d394cf4a86e
devsafix <devsafix@gmail.com>
2025-07-30 14:25:53 +0600

    block user service add

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,8 +1,26 @@
+import AppError from "../../errorHelpers/AppError";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
 
 const getAllUsers = async () => {
   const users = await User.find().select("-password");
   return users;
 };
 
-export const UserServices = { getAllUsers };
+const blockUser = async (userId: string) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  if (user.isBlocked) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is already blocked");
+  }
+
+  user.isBlocked = true;
+  await user.save();
+
+  return user;
+};
+
+export const UserServices = { getAllUsers, blockUser };
