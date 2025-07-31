73ca2b489c22c57bf56ae57be4412f0c33fda796
devsafix <devsafix@gmail.com>
2025-08-01 02:33:40 +0600

    user routes update with validation middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,11 @@
+// src/app/modules/user/user.route.ts
 import express from "express";
 import { UserControllers } from "./user.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { checkRole } from "../../middlewares/checkRole";
 import { userRoles } from "../../constants/role";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { userIdParamZodSchema } from "./user.validation";
 
 const router = express.Router();
 
@@ -16,24 +19,28 @@ router.patch(
   "/block/:id",
   checkAuth,
   checkRole(userRoles.ADMIN),
+  validateRequest(userIdParamZodSchema),
   UserControllers.block
 );
 router.patch(
   "/unblock/:id",
   checkAuth,
   checkRole(userRoles.ADMIN),
+  validateRequest(userIdParamZodSchema),
   UserControllers.unblock
 );
 router.patch(
   "/approve/:id",
   checkAuth,
   checkRole(userRoles.ADMIN),
+  validateRequest(userIdParamZodSchema),
   UserControllers.approve
 );
 router.patch(
   "/suspend/:id",
   checkAuth,
   checkRole(userRoles.ADMIN),
+  validateRequest(userIdParamZodSchema),
   UserControllers.suspend
 );
 
