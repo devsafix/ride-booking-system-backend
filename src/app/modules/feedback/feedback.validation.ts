30aaac1407f9bc3e501b53f5cd20bed0f95a20d4
devsafix <devsafix@gmail.com>
2025-08-01 11:00:42 +0600

    feedback and user validation updated

--- src/app/modules/feedback/feedback.validation.ts
+++ src/app/modules/feedback/feedback.validation.ts
@@ -1,20 +1,18 @@
 import { z } from "zod";
 
 export const createFeedbackZodSchema = z.object({
-  body: z.object({
-    rating: z
-      .number({
-        required_error: "Rating is required",
-        invalid_type_error: "Rating must be a number",
-      })
-      .int("Rating must be an integer")
-      .min(1, "Rating must be between 1 and 5")
-      .max(5, "Rating must be between 1 and 5"),
-    comment: z.string().optional(),
-    rideId: z
-      .string({
-        required_error: "Ride ID is required",
-      })
-      .nonempty("Ride ID cannot be empty"),
-  }),
+  rideId: z
+    .string({
+      required_error: "Ride ID is required",
+    })
+    .nonempty("Ride ID cannot be empty"),
+  rating: z
+    .number({
+      required_error: "Rating is required",
+      invalid_type_error: "Rating must be a number",
+    })
+    .int("Rating must be an integer")
+    .min(1, "Rating must be between 1 and 5")
+    .max(5, "Rating must be between 1 and 5"),
+  comment: z.string().optional(),
 });
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,25 +1,23 @@
 import { z } from "zod";
 
 export const createUserZodSchema = z.object({
-  body: z.object({
-    name: z
-      .string({
-        required_error: "Name is required",
-      })
-      .min(2, "Name must be at least 2 characters long"),
-    email: z
-      .string({
-        required_error: "Email is required",
-      })
-      .email("Invalid email format"),
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, "Password must be at least 6 characters long"),
-    role: z.enum(["rider", "driver", "admin"], {
-      required_error: "Role is required",
-      invalid_type_error: "Role must be 'rider', 'driver', or 'admin'",
-    }),
+  name: z
+    .string({
+      required_error: "Name is required",
+    })
+    .min(2, "Name must be at least 2 characters long"),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email("Invalid email format"),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, "Password must be at least 6 characters long"),
+  role: z.enum(["rider", "driver", "admin"], {
+    required_error: "Role is required",
+    invalid_type_error: "Role must be 'rider', 'driver', or 'admin'",
   }),
 });
